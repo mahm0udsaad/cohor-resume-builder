18491a75038b9c68c2196c61e76c841f141e8785
unknown <saad123mn123@gmail.com>
2024-11-13 06:30:19 +0200

    fix

--- app/[lng]/(withNav)/layout.js
+++ app/[lng]/(withNav)/layout.js
@@ -1,10 +1,6 @@
 import Nav from "@/components/component/nav";
 import "@/app/[lng]/globals.css";
 import { Footer } from "@/components/component/footer";
-
-export const icons = {
-  icon: "/favicon.ico",
-};
 export default function RootLayout({ children, params: { lng } }) {
   return (
     <>
